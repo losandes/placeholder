232a9e8d60097ee10f65942a13311409e80ee49f
losandes <andes.collab@gmail.com>
2018-08-08 16:42:25 -0400

    adds support for generating errors

--- app.js
+++ app.js
@@ -4,15 +4,41 @@ const serverId = Math.random().toString(36).substring(2, 15)
 console.log('starting app');
 
 const server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({
-      serverId,
-      path: req.url
-    }));
-    res.end();
+  if (req.url.indexOf('/error/') > -1) {
+    return errorHandler(req, res)
+  } else {
+    return defaultHandler(req, res)
+  }
 });
 
 var port = process.env.PORT || 3000;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+function defaultHandler (req, res) {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.write(JSON.stringify({
+    serverId,
+    path: req.url
+  }));
+  res.end();
+}
+
+function errorHandler (req, res) {
+  const errorIdx = req.url.indexOf('/error/')
+  const status = parseInt(req.url.substring(errorIdx + 7))
+
+  if (isNaN(status)) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+  } else {
+    res.writeHead(status, { "Content-Type": "application/json" });
+  }
+
+  res.write(JSON.stringify({
+    serverId,
+    path: req.url,
+    status: status
+  }));
+  res.end();
+}
